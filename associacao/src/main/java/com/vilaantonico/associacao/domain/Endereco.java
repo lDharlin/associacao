5077f72e494f36e84f2b35cd854b7bc000533f4d
Diogo Sales <diogosales.ads@gmail.com>
2026-05-07 10:00:31 -0300

    feat: cria entidade Endereco com relacionamento com Pessoa

---
+++ associacao/src/main/java/com/vilaantonico/associacao/domain/Endereco.java
@@ -0,0 +1,77 @@
+package com.vilaantonico.associacao.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco")
+    private Long idEndereco;
+
+    @Column(name = "tipo", length = 100, nullable = false)
+    private String tipo;
+
+    @Column(name = "rua", length = 50, nullable = false)
+    private String rua;
+
+    @Column(name = "numero", nullable = false)
+    private Integer numero;
+
+    @Column(name = "complemento", length = 50)
+    private String complemento;
+
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+
+    public Long getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Long idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+}
